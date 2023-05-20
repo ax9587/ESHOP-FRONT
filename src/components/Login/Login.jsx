7909ba7ac10342c2a168ed14efc6cdc530f2187c
ax9587 <ax9587@gmail.com>
2023-05-20 17:34:00 -0600

    err

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -31,6 +31,7 @@ const Login = () => {
         window.location.reload(true); 
       })
       .catch((err) => {
+        //toast.error(err.response.data.message);
         toast.error(err.response.data.message);
       });
   };
