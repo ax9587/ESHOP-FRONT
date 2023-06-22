5e0191cbca73c8d3d5976cc2060f906c8429e831
ax9587 <ax9587@gmail.com>
2023-06-21 21:11:20 -0600

    server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-//export const server = "https://eshop-server-gules.vercel.app/api/v2";
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://eshop-server-gules.vercel.app/api/v2";
+//export const server = "http://localhost:8000/api/v2";
 
-//export const backend_url = "https://eshop-server-gules.vercel.app/"
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "https://eshop-server-gules.vercel.app/"
+//export const backend_url = "http://localhost:8000/"
 
